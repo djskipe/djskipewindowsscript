c59195ed1a3fcbac2e6ec5babab9cbf11eddc2fd
djskipe <cris.bet97@gmail.com>
2025-08-06 13:48:45 +0200

    Update DJ SKIPE WINDOWS SCRIPT.bat

--- DJ SKIPE WINDOWS SCRIPT.bat
+++ DJ SKIPE WINDOWS SCRIPT.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Version information
-set "CURRENT_VERSION=1.7.0"
+set "CURRENT_VERSION=1.8.0"
 set "GITHUB_API_URL=https://api.github.com/repos/djskipe/djskipewindowsscript/releases/latest"
 
 :: Check for updates before showing the menu
@@ -116,7 +116,7 @@ echo:
 echo:       ______________________________________________________________
 echo:
 if "%LANG%"=="EN" (
-    echo:                 DJ SKIPE WINDOWS SCRIPT v1.7.0
+    echo:                 DJ SKIPE WINDOWS SCRIPT v1.8.0
     echo:
     echo          This script allows you to easily run the Windows
     echo          debloater from this CMD. It also allows you to
@@ -135,7 +135,7 @@ if "%LANG%"=="EN" (
     echo:             [9] Change Language
     echo:             [0] Exit
 ) else (
-    echo:                 DJ SKIPE WINDOWS SCRIPT v1.7.0
+    echo:                 DJ SKIPE WINDOWS SCRIPT v1.8.0
     echo:
     echo          Questo script ti permette di eseguire il debloater 
     echo          di Windows in facilita' direttamente da questo CMD.
